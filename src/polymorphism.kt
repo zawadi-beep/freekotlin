ee8c6c45e5e5ca53daef363ee062e8e781549b48
tess585 <tesswangui7@@gmail.com>
2024-03-14 13:00:55 +0300

    first commit

--- src/polymorphism.kt
+++ src/polymorphism.kt
@@ -1,26 +1,30 @@
 // parent class
-class shape{
-    fun draw(){
+open class shape{
+    open fun draw(){
         println("Drawing a shape")
     }
 }
 
-class Rhombus{
-    fun draw(){
+//child class
+class Rhombus:shape(){
+    override fun draw(){
         println("Drawing a rhombus")
     }
 }
 
-class Circle{
-    fun draw(){
+class Circle:shape(){
+    override fun draw(){
         println("Drawing a circle")
     }
 }
 
 fun main() {
     var s = shape()
+    s.draw()
     var r = Rhombus()
+    r.draw()
     var c = Circle()
+    c.draw()
 
 
 }
